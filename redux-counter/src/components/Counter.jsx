3a27e2c9f74f32190cac7fa5720f859f55936855
transformGit <2630243397@qq.com>
2016-11-18 22:32:58 +0800

    increamentIfOdd

--- redux-counter/src/components/Counter.jsx
+++ redux-counter/src/components/Counter.jsx
@@ -1,13 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 
-export default class Counter extends Component {
-	static propTypes: {
+const Counter = React.createClass({
+	propTypes: {
 		value: PropTypes.number.isRequired,
 		onIncrement: PropTypes.func.isRequired,
-		onDecrement: PropTypes.fucn.isRequired
-	}
+		onDecrement: PropTypes.func.isRequired
+	},
+	incrementIfOdd() {
+		if(this.props.value % 2 !== 0){
+			this.props.onIncrement()
+		}
+	},
 	render(){
-		const { value, onIncrement, onDecrement} = this.props
+		const { value, onIncrement, onDecrement, inCrementIfOdd} = this.props
 		return (
 			<p>
 				Clicked: {value} times
@@ -15,7 +20,10 @@ export default class Counter extends Component {
 				<button onClick={onIncrement}>add</button>
 				{' '}
 				<button onClick={onDecrement}>minus</button>
+				{' '}
+				<button onClick={this.incrementIfOdd}>Increment if odd</button>
 			</p>
 		)
 	}
-}
+})
+export default Counter;
