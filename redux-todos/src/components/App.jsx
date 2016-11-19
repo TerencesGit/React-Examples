040c24eacebee08bb641f6c2384f6a168aa9b7fe
transformGit <2630243397@qq.com>
2016-11-19 21:37:34 +0800

    AddTodo

--- redux-todos/src/components/App.jsx
+++ redux-todos/src/components/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from 'react'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
 const App = () => (
-		<div>
-			<h1>hello redux</h1>
-		</div>
+	<div>
+		<AddTodo />
+		<VisibleTodoList />
+	</div>
 )
-export default App;
+export default App
--- redux-todos/src/index.jsx
+++ redux-todos/src/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 render(
-	<div>
+	<Provider store={store}>
 		<App />
-	</div>,
+	</Provider>,
 	document.getElementById('root')
 )
